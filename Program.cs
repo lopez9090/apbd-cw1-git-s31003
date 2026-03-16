b385ab6752fe98e27526bd8710c014a926989a1a
Jan Myziak <s31003@pjwstk.edu.pl>
2026-03-16 15:46:52 +0100

    Dodano obsluge bledu w Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using System;
 Console.WriteLine("Podaj liczby oddzielone spacja:");
 string input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input)) 
+{
+    Console.WriteLine("Nie podano zadnych danych");
+    return;
+}
